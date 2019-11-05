2539d6e480ae9e8e9caabd2aa8a12ce364d137c2
Brad Rydzewski <brad.rydzewski@gmail.com>
2019-11-05 12:48:48 -0800

    use annotations for downward API values

--- engine/engine_impl.go
+++ engine/engine_impl.go
@@ -208,11 +208,11 @@ func (k *Kubernetes) start(spec *Spec, step *Step) error {
 		for i, container := range pod.Spec.Containers {
 			if container.Name == step.ID {
 				pod.Spec.Containers[i].Image = step.Image
-				if pod.ObjectMeta.Labels == nil {
-					pod.ObjectMeta.Labels = map[string]string{}
+				if pod.ObjectMeta.Annotations == nil {
+					pod.ObjectMeta.Annotations = map[string]string{}
 				}
 				for _, env := range statusesWhiteList {
-					pod.ObjectMeta.Labels[env] = step.Envs[env]
+					pod.ObjectMeta.Annotations[env] = step.Envs[env]
 				}
 			}
 		}

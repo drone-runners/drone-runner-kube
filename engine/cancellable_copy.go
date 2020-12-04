5dd73117bb15c527c2b4534844a0b1fbc702e57f
Shubham Agrawal <shubham.agrawal@harness.io>
2020-12-04 11:46:02 +0530

    Fix goleak in cancellable copy

--- engine/cancellable_copy.go
+++ engine/cancellable_copy.go
@@ -10,8 +10,9 @@ import (
 // cancellableCopy method copies from source to destination honoring the context.
 // If context.Cancel is called, it will return immediately with context cancelled error.
 func cancellableCopy(ctx context.Context, dst io.Writer, src io.ReadCloser) error {
-	ch := make(chan error)
+	ch := make(chan error, 1)
 	go func() {
+		defer close(ch)
 		err := livelog.Copy(dst, src)
 		ch <- err
 	}()

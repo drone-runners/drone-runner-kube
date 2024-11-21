f90bed3e496357fb8dd354c412a706a30aaa8661
ShobhitSingh11 <shobhit.singh@harness.io>
2024-11-21 15:12:45 +0530

    fix: [CI-14780]: Fixing secret masking issue in Kube Runner

--- engine/compiler/compiler.go
+++ engine/compiler/compiler.go
@@ -497,11 +497,7 @@ func (c *Compiler) Compile(ctx context.Context, args runtime.CompilerArgs) runti
 		}
 
 		for _, s := range step.Secrets {
-			// if the secret was already fetched and stored in the
-			// secret map it can be skipped.
-			if _, ok := spec.Secrets[s.Name]; ok {
-				continue
-			}
+
 			secret, ok := c.findSecret(ctx, args, s.Name)
 			if ok {
 				s := &engine.Secret{
